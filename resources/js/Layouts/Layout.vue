a7fedc9f2079087b48dd3a8cf3f292a2cffbbe92
yaboo <yaboonjapan@gmail.com>
2024-01-08 02:21:18 +0900

    Added Sample Logo

--- resources/js/Layouts/Layout.vue
+++ resources/js/Layouts/Layout.vue
@@ -15,34 +15,39 @@ const isGuest = page.props.isGuest
     <Head title="Aimat" />
 
     <v-layout class="rounded rounded-md">
-        <v-app-bar title="Logo here">
-            <v-menu>
-                <template v-slot:activator="{ props }">
-                    <v-icon icon="mdi-menu" v-bind="props" class="mr-5"></v-icon>
-                </template>
-                <v-list>
-                    <v-list-item v-if="isGuest">
-                        <v-list-item-title>
-                            <Link :href="route('login')">Log in</Link>
-                        </v-list-item-title>
-                    </v-list-item>
-                    <v-list-item v-if="isGuest">
-                        <v-list-item-title>
-                            <Link :href="route('register')">Register</Link>
-                        </v-list-item-title>
-                    </v-list-item>
-                    <v-list-item v-if="!isGuest">
-                        <v-list-item-title>
-                            <button @click="logout()">Log out</button>
-                        </v-list-item-title>
-                    </v-list-item>
-                    <v-list-item v-if="!isGuest">
-                        <v-list-item-title>
-                            <Link :href="route('profile.edit')">Profile</Link>
-                        </v-list-item-title>
-                    </v-list-item>
-                </v-list>
-            </v-menu>
+        <v-app-bar height="80">
+            <v-row justify="start">
+                <v-img src="/images/logo.png" max-width="150px" max-height="100" class="ml-7" />
+            </v-row>
+            <v-row justify="end">
+                <v-menu>
+                    <template v-slot:activator="{ props }">
+                        <v-icon icon="mdi-menu" v-bind="props" class="mr-5" size="40"></v-icon>
+                    </template>
+                    <v-list>
+                        <v-list-item v-if="isGuest">
+                            <v-list-item-title>
+                                <Link :href="route('login')">Log in</Link>
+                            </v-list-item-title>
+                        </v-list-item>
+                        <v-list-item v-if="isGuest">
+                            <v-list-item-title>
+                                <Link :href="route('register')">Register</Link>
+                            </v-list-item-title>
+                        </v-list-item>
+                        <v-list-item v-if="!isGuest">
+                            <v-list-item-title>
+                                <button @click="logout()">Log out</button>
+                            </v-list-item-title>
+                        </v-list-item>
+                        <v-list-item v-if="!isGuest">
+                            <v-list-item-title>
+                                <Link :href="route('profile.edit')">Profile</Link>
+                            </v-list-item-title>
+                        </v-list-item>
+                    </v-list>
+                </v-menu>
+            </v-row>
         </v-app-bar>
 
         <v-main class="d-flex align-center justify-center" style="min-height: 300px;">
