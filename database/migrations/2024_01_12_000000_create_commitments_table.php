1570194c4226060204088778bdac13bc5ae2a481
yaboo <yaboonjapan@gmail.com>
2024-01-12 19:04:05 +0900

    Created migration files for task

---
+++ database/migrations/2024_01_12_000000_create_commitments_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('commitments', function (Blueprint $table) {
+            $table->id();
+            $table->integer('goal_id');
+            $table->integer('period_type');
+            $table->string('target');
+            $table->integer('target_level');
+            $table->integer('target_level_unit_type');
+            $table->string('note');
+            $table->timestamp('last_used_at')->nullable();
+            $table->timestamp('expires_at')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('commitments');
+    }
+};
---
+++ database/migrations/2024_01_12_000000_create_tasks_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->integer('commitment_id');
+            $table->string('note');
+            $table->integer('progress');
+            $table->boolean('is_archived');
+            $table->timestamp('last_used_at')->nullable();
+            $table->timestamp('expires_at')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('personal_access_tokens');
+    }
+};
